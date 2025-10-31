86c57c89b2acd5e07f90d73736bb63588d31d247
JC-Cola <jc.cola2020@gmail.com>
2025-10-31 09:03:33 -0400

    sanitize_filename done

--- 2 - Collections Essentials/Part 2 - Strings/practice2.py
+++ 2 - Collections Essentials/Part 2 - Strings/practice2.py
@@ -9,3 +9,24 @@ print(format_phone_number("555-123-4567"))
 print(format_phone_number("(555) 123 4567"))
 print(format_phone_number("5551234567"))
 print(format_phone_number("123"))
+
+def sanitize_filename(filename):
+    clean_filename = filename.lower().replace(" ", "_")
+    special = "._-"
+    for char in clean_filename:
+        if not char.isdigit():
+            if not char.isalpha():
+                if char not in special:
+                    clean_filename = clean_filename.replace(char, "")
+    if clean_filename[-4:] != ".txt":
+        clean_filename += ".txt"
+    
+    if len(clean_filename) > 46:
+        return "50 char max with .txt"
+    
+    return clean_filename   
+
+print(sanitize_filename("Ancient Scroll.txt"))
+print(sanitize_filename("Quest 2024! (Epic)"))
+print(sanitize_filename("notes"))
+print(sanitize_filename("X"*60))
