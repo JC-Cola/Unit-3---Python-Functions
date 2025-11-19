e6d132f121c28472b96a68e8ab81b4353f355e13
JC-Cola <jc.cola2020@gmail.com>
2025-11-19 09:52:15 -0500

    returns

--- 3 - Parameters/b - args-kwargs/returns.py
+++ 3 - Parameters/b - args-kwargs/returns.py
@@ -18,14 +18,56 @@ print(not result) #True - falsy check
 #2 Return Type - False = Boolean False
 #Meaning: Explicit false condition, validation failure, negative result
 #Use for: Validation result, boolean operations, success/failure status
-result = False
-print(result is False) #True - identity check
-print(not result) #True - boolean negation
-print(result == 0) #True - falsy check
+result1 = False
+print(result1 is False) #True - identity check
+print(not result1) #True - boolean negation
+print(result1 == 0) #True - falsy check
 
 #3 Return Zero - A Valid Number
-result = 0
-print(result = 00) #True - numeric equality
-print(not result) #True - falsy in boolean context
-print(result is None) #False - different objects
-print(result is False) #False - different objects
+result2 = 0
+print(result2 == 00) #True - numeric equality
+print(not result2) #True - falsy in boolean context
+print(result2 is None) #False - different objects
+print(result2 is False) #False - different types
+#Multiple Returns- python packs multiple returns into a tuple!
+def calculate_room(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter #Turns into a tuple
+a = calculate_room(10, 5)
+print(a)
+print(type(a))
+
+print(type((42))) #int
+print(type((42,))) #tuple
+no_parentheses = 1,2,3
+print(type(no_parentheses)) #tuple
+
+#Unpacking tuple
+area_result, perimeter_result = calculate_room(20, 6)
+print(f"Area: {area_result}, Perimeter: {perimeter_result}")
+
+#Build a function that returns multiple grades
+def analyze_grades(grades):
+    # if not grades:
+    #     return 0, 0, 0, False
+    # average = sum(grades)/len(grades)
+    # highest = max(grades)
+    # lowest = min(grades)
+    # if average > 60:
+    #     passed = True
+    # else:
+    #     passed = False
+    # return average, highest, lowest, passed
+    
+    #Gemici Way
+    if not grades:
+        return 0,0,0,False
+    average = sum(grades)/len(grades)
+    highest = max(grades)
+    lowest = min(grades)
+    passed = average >= 60
+    return average, highest, lowest, passed
+print(analyze_grades([85, 92, 78, 90])) #[86.25, 92, 78, True]
+print(analyze_grades([])) #[0, 0, 0, False]
+print(analyze_grades([80, 80, 80])) #[80.0, 80, 80, True]
