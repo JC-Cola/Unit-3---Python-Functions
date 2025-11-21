e3af3c7eca80677fc45a602d1bf03222cfe18008
JC-Cola <jc.cola2020@gmail.com>
2025-11-21 08:56:02 -0500

    exceptions

---
+++ 5 - Exception Handling/common-exceptions.py
@@ -0,0 +1,38 @@
+# Common Errors
+# 1 - Division by zero
+# print(10/0)
+
+# 2 - Value Error
+# print(int("abc"))
+
+# 3 - Key Error
+# grades = {"Bob":90}
+# print(grades["Bill"])
+
+# 4 - Index Error
+# numbers = [1, 2, 3]
+# print(numbers[5])
+
+# 5 - Name Error
+# print(total)
+# total = 10
+
+# 6 - Type Error
+# print("hello" + 5)
+
+# 7 - Syntax Error
+# for i in range(5)
+# print(i)
+
+# 7 - 
+
+# 8 - Attribute Error
+# x = 5
+# x.append(3)
+
+# 9 - ModuleNotFoundError
+# import wonka_factor
+
+# # 10 - IndentationError (cannot run directly, will crash)
+# if True:
+# print("Hi")
--- 5 - Exception Handling/exceptions.py
+++ 5 - Exception Handling/exceptions.py
@@ -1,22 +1,58 @@
-# Common Errors
-# 1 - Division by zero
-# print(10/0)
+def safe_divide(a,b):
+    try:
+        return a / b
+    # except:
+    #     print("Can not divide by zero!")
+    #     return None
+    except ZeroDivisionError:
+        print("Can not divide by zero!")
+        return None
+    except TypeError:
+        print("That's not a valid number!")
+        return None
+    except:
+        print("An error occured...")
+print(safe_divide(10,2)) # 5.0
+print(safe_divide(10,0)) # Can not divide by zero! Returns None
+print(safe_divide(10,"hello")) # Can not divide by zero! Returns None
 
-# 2 - Value Error
-# print(int("abc"))
+def safe_operations(a,b,lst,key,d):
+    try:
+        print(f"Division Result: {a/b}")
+        print("Access list element", {lst[2]})
+        print("Access dictionary value", {d[key]})
+        print("Add numbers: ", a + b)
+    except ZeroDivisionError:
+        print("Can not divide by zero!")
+    except IndexError:
+        print("List index out of range!")
+    except KeyError:
+        print(f"Key {key} not found in dictionary!")
+    except TypeError:
+        print("Invalid types for operation!")
+    except Exception as e:
+        print("Some other error occured")
+        
+print(safe_operations(10,2,[1,2],"Tom",{"John":15}))
+print(safe_operations(10,0,[1,2],"Tom",{"John":15}))
+print(safe_operations(10,0,[1,2],"Tom",{"Tom":15}))
 
-# 3 - Key Error
-# grades = {"Bob":90}
-# print(grades["Bill"])
+def calculate_price_per_item(total, items):
+    try:
+        price_per_item = total/items
+        return round(price_per_item, 2)
+    except ZeroDivisionError:
+        return "No items to calculate"
 
-# 4 - Index Error
-# numbers = [1, 2, 3]
-# print(numbers[5])
+print(calculate_price_per_item(100, 4)) 
+print(calculate_price_per_item(50, 0))
+print(calculate_price_per_item(25.50, 3))
 
-# 5 - Name Error
-# print(total)
-# total = 10
-
-# Syntax Error
-# for i in range(5)
-# print(i)
+def parse_age(str):
+    try:
+        age = int(str)
+        return age
+    except ValueError:
+        return None
+print(parse_age("25"))
+print(parse_age("twenty-five"))
